019482100ccdac35201f61f651e48e4f9e795a12
Antoine Dumez <MY_NAME@example.com>
2022-05-23 00:04:46 +0200

    ok

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -6,13 +6,24 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/program', name: 'program_')]
 class ProgramController extends AbstractController
 {
-    #[Route('/program', name: 'app_program')]
+    #[Route('/', name: 'index')]
     public function index(): Response
     {
-        return $this->render('program/index.html.twig', [
-            'website' => 'Wild Series',
-        ]);
+        if ($_POST){
+           return $this->redirectToRoute('program_show', ['id' => $_POST['id']]);
+        }
+        return $this->render('program/index.html.twig');
     }
+
+
+    #[Route('/{id<\d+>}/', name: 'show', methods: 'GET')]
+    public function show($id = 1): Response
+    {
+        return $this->render('program/show.html.twig', [
+            'id' => $id,
+        ]);
+     }
 }
